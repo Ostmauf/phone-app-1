a89abc288958a860b0dfd5c2b468438c7f7f4b43
Ostmauf <71626781+Ostmauf@users.noreply.github.com>
2021-10-06 10:15:02 +0300

    this i desant

--- public/app.js
+++ public/app.js
@@ -1,13 +1,28 @@
 const chat = document.getElementById("chat-window");
 const chatButton = document.getElementById("chat-button");
 const chatInput = document.getElementById("chat-input");
+const username = "Sebastian"
 
 chatButton.addEventListener("click", onChatButtonClicked);
 
 function onChatButtonClicked() {
+   if (chatInput.value == "" || chatInput.value == " ") {
+    //Do nothing.
+   } else {
+      const div1 = document.createElement("div");
+      // styling div
+      div1.style.marginBottom = "1rem"
+      div1.style.backgroundColor = "yellow"
+      div1.style.border = "solid darkyellow 1px"
+      div1.style.borderRadius = "0.5rem"
+      div1.style.padding = "0.5rem"
 
-    chat.innerHTML += "<br>" + chatInput.value
 
+      chat.append(div1);
+      div1.append(`From ${username}: ${chatInput.value}`);
+      chat.innerHTML += "<br>";
+      chatInput.value = "";
+   }
 
-    chatInput.value = ""
+    
 }

51a435a3641616146c4c18d3119eb7e6254a571e
Ostmauf <71626781+Ostmauf@users.noreply.github.com>
2021-10-29 11:04:26 +0300

    fixed more

--- public/app.js
+++ public/app.js
@@ -1,53 +1,55 @@
 const chat = document.getElementById("chat-window");
 const chatButton = document.getElementById("chat-button");
 const chatInput = document.getElementById("chat-input");
-const username = "Sebastian"
+const username = "Sebastian";
 
 chatButton.addEventListener("click", onChatButtonClicked);
 
 function onChatButtonClicked() {
    if (chatInput.value == "" || chatInput.value == " ") {
-    //Do nothing.
+    // Do nothing.
    } else {
+      // container to message holder.
       const div1 = document.createElement("div");
 
+      // creating were it displays who sent the message and the message.
       const fromMessage = document.createElement("span");
       const userMessage = document.createElement("span");
 
-      div1.setAttribute("id", "user-message-and-were-it-is-from");
-      const div1New = document.getElementById("user-message-and-were-it-is-from");
-      
+      // putting who sent message and message to div1.
+      div1.append(fromMessage, userMessage);
 
-      chat.append(div1New);
-      div1.append(fromMessage, userMessage)
+      //adding content to fromMesage and userMessage.
       fromMessage.append(`From ${username}: `);
       userMessage.append(chatInput.value);
-      
-      // Styling div
-      div1New.style.minWidth = "35%";
-      div1New.style.width = "fit-content";
-      div1New.style.minHeight = "1rem";
-      div1New.style.backgroundColor = "yellow";
-      div1New.style.border = "solid darkyellow 1px";
-      div1New.style.borderRadius = "0.5rem";
-      div1New.style.padding = "0.5rem 0.5rem 0.5rem 0";
-      div1New.style.display = "flex";
-      div1New.style.alignItems = "center";
-      
-      // Styling fromMessage
+
+      // putting message container in the chat.
+      chat.append(div1);
+
+      // Styling div1.
+      div1.style.minWidth = "35%";
+      div1.style.width = "fit-content";
+      div1.style.minHeight = "1rem";
+      div1.style.backgroundColor = "yellow";
+      div1.style.border = "solid darkyellow 1px";
+      div1.style.borderRadius = "0.5rem";
+      div1.style.padding = "0.5rem 0.5rem 0.5rem 0";
+      div1.style.display = "flex";
+      div1.style.alignItems = "center";
+
+      // Styling fromMessage.
       fromMessage.style.fontFamily = "'Merriweather', serif"
       fromMessage.style.fontWeight = "900";
       fromMessage.style.fontStyle = "italic";
 
-      // Styling userMessage
+      // Styling userMessage.
       userMessage.style.fontFamily = "'Merriweather', serif";
       userMessage.style.fontSize = "smaller";
-      
-      
-      
 
-
+      // makes newline to the chat after message sent.
       chat.innerHTML += "<br>";
+
+      // takes away chat message away from the input after message is sent.
       chatInput.value = "";
    }
 

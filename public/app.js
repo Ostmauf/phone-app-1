bb73ae4199fbf8fafc4012a93df75230f56071c7
Ostmauf <71626781+Ostmauf@users.noreply.github.com>
2021-10-29 08:31:22 +0300

    needs fix

--- public/app.js
+++ public/app.js
@@ -10,16 +10,43 @@ function onChatButtonClicked() {
     //Do nothing.
    } else {
       const div1 = document.createElement("div");
-      // styling div
-      div1.style.marginBottom = "1rem"
-      div1.style.backgroundColor = "yellow"
-      div1.style.border = "solid darkyellow 1px"
-      div1.style.borderRadius = "0.5rem"
-      div1.style.padding = "0.5rem"
+
+      const fromMessage = document.createElement("span");
+      const userMessage = document.createElement("span");
+
+      div1.setAttribute("id", "user-message-and-were-it-is-from");
+      const div1New = document.getElementById("user-message-and-were-it-is-from");
+      
+
+      chat.append(div1New);
+      div1.append(fromMessage, userMessage)
+      fromMessage.append(`From ${username}: `);
+      userMessage.append(chatInput.value);
+      
+      // Styling div
+      div1New.style.minWidth = "35%";
+      div1New.style.width = "fit-content";
+      div1New.style.minHeight = "1rem";
+      div1New.style.backgroundColor = "yellow";
+      div1New.style.border = "solid darkyellow 1px";
+      div1New.style.borderRadius = "0.5rem";
+      div1New.style.padding = "0.5rem 0.5rem 0.5rem 0";
+      div1New.style.display = "flex";
+      div1New.style.alignItems = "center";
+      
+      // Styling fromMessage
+      fromMessage.style.fontFamily = "'Merriweather', serif"
+      fromMessage.style.fontWeight = "900";
+      fromMessage.style.fontStyle = "italic";
+
+      // Styling userMessage
+      userMessage.style.fontFamily = "'Merriweather', serif";
+      userMessage.style.fontSize = "smaller";
+      
+      
+      
 
 
-      chat.append(div1);
-      div1.append(`From ${username}: ${chatInput.value}`);
       chat.innerHTML += "<br>";
       chatInput.value = "";
    }
